3407575c02b540708582dc6159618adef9d697a9
Jonathan Smith <jonathan@peoplesized.com>
2020-01-13 15:55:45 +0200

    rename, add interfaces

--- src/index.ts
+++ src/index.ts
@@ -20,14 +20,14 @@ exports.get_data = async (_: any, res: express.Response) => {
 
 async function fetch_and_combine() {
   try {
-    const static_info = await fetch_airtable();
-    const fn_status = await fetch_openfaas();
+    const airtable_data = await fetch_airtable();
+    const openfaas_data = await fetch_openfaas();
 
     // TODO: Remove debug files
-    // fs.writeFileSync('static_info.json', JSON.stringify(static_info))
-    // fs.writeFileSync('fn_status.json', JSON.stringify(fn_status))
+    // fs.writeFileSync('airtable_data.json', JSON.stringify(airtable_data))
+    // fs.writeFileSync('openfaas_data.json', JSON.stringifyopenfaas_datafn_status))
 
-    return combine(static_info, fn_status);
+    return combine(airtable_data, openfaas_data);
   } catch (e) {
     throw new Error(
       "Cannot get OpenFaas or Airtable data - check connections and/or credentials?"
@@ -35,7 +35,15 @@ async function fetch_and_combine() {
   }
 }
 
-async function fetch_airtable() {
+interface AirtableRecord {
+  function_name: string;
+  image_version: string;
+  repo: string;
+  scale_to_zero: boolean;
+  hide_from_deploy: boolean;
+}
+
+async function fetch_airtable(): Promise<AirtableRecord[]> {
   const url = "https://api.airtable.com/v0/app2A1oMnkLm1B747/algos";
   const AIRTABLE_KEY = process.env.AIRTABLE_KEY;
   const headers = { Authorization: AIRTABLE_KEY };
@@ -48,7 +56,15 @@ async function fetch_airtable() {
   return [];
 }
 
-async function fetch_openfaas() {
+interface OpenFaasRecord {
+  name: string;
+  image: string;
+  invocationCount: number;
+  replicas: number;
+  availableReplicas: number;
+}
+
+async function fetch_openfaas(): Promise<OpenFaasRecord[]> {
   //  Make request
   const url = "https://faas.srv.disarm.io/system/functions";
   const OPENFAAS_KEY = process.env.OPENFAAS_KEY;
@@ -65,11 +81,11 @@ async function fetch_openfaas() {
   }
 }
 
-function combine(static_info: any[], fn_status: any[]) {
+function combine(airtable_data: any[], openfaas_data: any[]) {
 
-  return static_info.map((f) => {
+  return airtable_data.map((f) => {
     const remote_image = get(
-      find_image_by_name(f.function_name, fn_status),
+      find_image_by_name(f.function_name, openfaas_data),
       "image",
       "no image"
     );
@@ -79,6 +95,6 @@ function combine(static_info: any[], fn_status: any[]) {
   });
 }
 
-function find_image_by_name(name: string, fn_status: any[]) {
-  return fn_status.find((s) => s.function_name === name);
+function find_image_by_name(name: string, openfaas_data: any[]) {
+  return openfaas_data.find((s) => s.function_name === name);
 }

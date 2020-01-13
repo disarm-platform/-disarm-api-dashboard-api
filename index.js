86883aecb101b856ea0a5a7f9d935d95e83686d7
Jonathan Smith <jonathan@peoplesized.com>
2020-01-13 14:26:00 +0200

    betterer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const get = require("lodash").get;
 const axios = require("axios");
+const fs = require('fs');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -10,13 +11,21 @@ exports.get_data = function (req, res) {
     .then(data => {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(data));
+    })
+    .catch(e => {
+      console.error(e);
     });
 };
 
 async function fetch_and_combine() {
   try {
-    const static_info = await fetch_static_info();
-    const fn_status = await fetch_openfaas_functions();
+    const static_info = await fetch_airtable();
+    const fn_status = await fetch_openfaas();
+
+    // TODO: Remove debug files
+    fs.writeFileSync('static_info.json', JSON.stringify(static_info))
+    fs.writeFileSync('fn_status.json', JSON.stringify(fn_status))
+
     return combine(static_info, fn_status);
   } catch (e) {
     throw new Error(
@@ -25,42 +34,29 @@ async function fetch_and_combine() {
   }
 }
 
-async function fetch_static_info() {
+async function fetch_airtable() {
   const url = "https://api.airtable.com/v0/app2A1oMnkLm1B747/algos";
   const AIRTABLE_KEY = process.env.AIRTABLE_KEY;
   const headers = { Authorization: AIRTABLE_KEY };
   const res = await axios.get(url, { headers });
   const records = get(res, "data.records", []);
   if (records) {
-    return records.map(record => normalise_static_fields(record.fields));
+    return records.map(record => record.fields);
   }
 
   return [];
 }
 
-function normalise_static_fields(fields) {
-  return {
-    function_name: get(fields, "function_name", "no function - impossible?"),
-    repo: get(fields, "repo", "none - weird"),
-    ui: get(fields, "ui", "none"),
-    ui_repo: get(fields, "ui_repo", "none"),
-    uses: get(fields, "uses", "no registered uses"),
-    dev_notes: get(fields, "dev_notes", "none"),
-  };
-}
-
-async function fetch_openfaas_functions() {
+async function fetch_openfaas() {
   //  Make request
   const url = "https://faas.srv.disarm.io/system/functions";
   const OPENFAAS_KEY = process.env.OPENFAAS_KEY;
-  const headers = {
-    Authorization: OPENFAAS_KEY
-  };
+  const headers = { Authorization: OPENFAAS_KEY };
   try {
     const res = await axios.get(url, { headers });
     //  Check response
     if (res.data && res.data.length > 0) {
-      return res.data.map(fields => normalise_status_fields(fields));
+      return res.data.map(fields => fields);
     }
     throw new Error("Missing data from OpenFaas request");
   } catch (e) {
@@ -68,14 +64,8 @@ async function fetch_openfaas_functions() {
   }
 }
 
-function normalise_status_fields(fields) {
-  return {
-    function_name: get(fields, "name", "no function"),
-    image: get(fields, "image", "no image")
-  };
-}
-
 function combine(static_info, fn_status) {
+
   return static_info.map(f => {
     const remote_image = get(
       find_image_by_name(f.function_name, fn_status),

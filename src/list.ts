0fbcfce317a03df5de1bebd58b37756cab41e55b
Jonathan Smith <jonathan@peoplesized.com>
2020-01-29 13:28:33 +0200

    deployable! (and built)

--- src/list.ts
+++ src/list.ts
@@ -28,8 +28,8 @@ async function fetch_and_combine() {
   try {
     airtable_data = await fetch_airtable();
     openfaas_data = await fetch_openfaas();
-    fs.writeFileSync('sample_responses/airtable_data.json', JSON.stringify(airtable_data));
-    fs.writeFileSync('sample_responses/openfaas_data.json', JSON.stringify(openfaas_data));
+  //   fs.writeFileSync('sample_responses/airtable_data.json', JSON.stringify(airtable_data));
+  //   fs.writeFileSync('sample_responses/openfaas_data.json', JSON.stringify(openfaas_data));
     // airtable_data = JSON.parse(fs.readFileSync('sample_responses/airtable_data.json', 'utf8'));
     // openfaas_data = JSON.parse(fs.readFileSync('sample_responses/openfaas_data.json', 'utf8'));
   } catch (e) {

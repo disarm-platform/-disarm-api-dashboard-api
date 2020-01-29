d88b1ca4d5407e4f85cb1ca47500d72d79797cfe
superuser-chi <mavusogiftntokozo@gmail.com>
2020-01-29 10:23:30 +0200

    add cors headers

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ import { deploy, undeploy } from './actions';
 exports.get_data = async (req: express.Request, res: express.Response) => {
   const [, command, function_name] = req.path.split('/'); // Yup, need that first comma!
 
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST');
+  res.set('Access-Control-Allow-Headers', 'content-type');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+    return;
+  }
   switch (command) {
     case '':
       list(req, res);

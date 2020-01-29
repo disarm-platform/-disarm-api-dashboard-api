9e209722a4cc3118a3852acce37ca2ebeca75baa
Jonathan Smith <jonathan@peoplesized.com>
2020-01-29 13:05:11 +0200

    update Airtable URL - use new table

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export const CONFIG = {
-  airtable_url: process.env.AIRTABLE_URL || 'https://api.airtable.com/v0/app2A1oMnkLm1B747/algos',
+  airtable_url: process.env.AIRTABLE_URL || 'https://api.airtable.com/v0/appUekRZWqLHK31zF/Algos',
   airtable_key: process.env.AIRTABLE_KEY,
   openfaas_url: process.env.OPENFAAS_URL || 'https://faas.srv.disarm.io',
   openfaas_key: process.env.OPENFAAS_KEY
